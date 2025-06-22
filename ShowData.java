b2d979e63c678424d907ba458d80c53c07ef5102
c.sonoyama <sonoyama.tech@gmail.com>
2025-06-22 21:12:59 +0900

    initial commit(2-1～2-5)

---
+++ ShowData.java
@@ -0,0 +1,20 @@
+class ShowData {
+  public static void main(String[] args) {
+    // 変数の宣言と代入
+    int num;
+    num = 10;
+    double pi;
+    pi = 3.14;
+    char blood = 'A';
+    boolean fun;
+    fun = true;
+    String msg = "メッセージ";
+
+    // 値の表示
+    System.out.println("num：" + num);
+    System.out.println("pi：" + pi);
+    System.out.println("blood：" + blood);
+    System.out.println("fun：" + fun);
+    System.out.println("msg：" + msg);
+  }
+}
---
+++ Variable.java
@@ -0,0 +1,11 @@
+class Variable {
+  public static void main(String[] args) {
+    int year;
+    year = 2025;
+    int month = 6;
+    int day = 22;
+
+    System.out.println("この処理は");
+    System.out.println(year + "年" + month + "月" + day +"日に作成しました。");
+  }
+}
