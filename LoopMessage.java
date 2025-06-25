9a27510362f7648f65f0b28962a03477b95a7b97
c.sonoyama <sonoyama.tech@gmail.com>
2025-06-25 16:15:17 +0900

    2-6～2-10

---
+++ LoopMessage.java
@@ -0,0 +1,10 @@
+class LoopMessage {
+  public static void main(String[] args) {
+    // メッセージを10回表示
+    int i = 0;
+    while (i < 10) {
+      System.out.println((i + 1) + "回目：フリーラーニングを広げたい！");
+      i++;
+    }
+  }
+}
